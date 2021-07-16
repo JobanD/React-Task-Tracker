0f4485a3b4495b40a4415a8f6c2b313b52242a0d
JobanD <joban.d555@gmail.com>
2021-07-16 15:57:07 -0400

    Finishing up simple task tracker project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAddTask }) => {
   const onClick = () => {
     console.log("Click");
   };
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button
+        color={showAddTask ? "red" : "green"}
+        text={showAddTask ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
